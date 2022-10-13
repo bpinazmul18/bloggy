5a0f7dd944e8af67bbf211522355e99c17fac0d1
Nazmul Haque <bpinazmul18@gmail.com>
2022-10-13 08:04:54 +0600

    Add comment time.

--- src/components/common/CommentItem.jsx
+++ src/components/common/CommentItem.jsx
@@ -7,7 +7,10 @@ export default function CommentItem({ comment }) {
           <span className="top-0 left-7 absolute  w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"></span>
         </div>
         <div className="text-left">
-          <div className="text-xl font-medium text-black">{comment.name}</div>
+          <div className="flex items-center space-x-2">
+            <div className="text-xl font-medium text-black">{comment.name}</div>
+            <div className="text-sm text-gray">Today 6:03 pm</div>
+          </div>
           <p className="text-slate-500">{comment.text}</p>
         </div>
       </div>
